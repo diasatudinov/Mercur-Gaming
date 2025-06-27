ce0e28198403b815e7f66f1540e0af7c53ea65b8
Dias <dias.atudinov@gmail.com>
2025-06-27 17:05:56 +0500

    menu and splash view

--- Mercur Gaming/Mercur Gaming/MEGViews/MEGMenu/MEGMenuView.swift
+++ Mercur Gaming/Mercur Gaming/MEGViews/MEGMenu/MEGMenuView.swift
@@ -2,16 +2,151 @@
 //  MEGMenuView.swift
 //  Mercur Gaming
 //
-//  Created by Dias Atudinov on 27.06.2025.
 //
 
 import SwiftUI
 
 struct MEGMenuView: View {
+    @State private var showGame = false
+    @State private var showAchievement = false
+    @State private var showShop = false
+    @State private var showDailyTask = false
+    @State private var showDailyReward = false
+    
+    //    @StateObject var achievementVM = ITTPAchievementsViewModel()
+    //    @StateObject var settingsVM = ITTPSettingsViewModel()
+    //    @StateObject var gameVM = ITTPNewGameViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            VStack(spacing: 0) {
+                HStack {
+                    Button {
+                        showDailyReward = true
+                        
+                    } label: {
+                        
+                        ZStack {
+                            Image(.dailyRewardMEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 100:50)
+                            
+                        }
+                    }
+                    Spacer()
+                    
+                    Button {
+                        showDailyTask = true
+                        
+                    } label: {
+                        
+                        ZStack {
+                            Image(.dailyTaskIconMEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 100:50)
+                            
+                        }
+                    }
+                    
+                }
+                
+                VStack(spacing: 30) {
+                    VStack {
+                        Image(.welcomeTextMEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 30:15)
+                        
+                        Image(.cozyBearTextMEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 50:25)
+                    }
+                    VStack(spacing: 15) {
+                        Image(.dailyTaskImageMEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 100:50)
+                        Image(.dailyTaskTextMEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 20:10)
+                    }
+                }.padding(.top, 30).padding(.bottom, 28)
+                
+                VStack(spacing: 20) {
+                    Button {
+                        showGame = true
+                        
+                    } label: {
+                        
+                        ZStack {
+                            Image(.playIconMEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 130:80)
+                            
+                        }
+                    }
+                    
+                    Button {
+                        showAchievement = true
+                    } label: {
+                        ZStack {
+                            Image(.achievementsIconMEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 130:80)
+                            
+                        }
+                    }
+                    
+                    
+                    
+                    Button {
+                        showShop = true
+                    } label: {
+                        Image(.shopIconMEG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 130:80)
+                    }
+                    
+                }
+                Spacer()
+            }.padding(.horizontal)
+            
+            
+        }.padding()
+            .background(
+                ZStack {
+                    Image(.appBgMEG)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                }
+            )
+            .fullScreenCover(isPresented: $showGame) {
+                //                ITTPNewGameView(viewModel: gameVM)
+            }
+            .fullScreenCover(isPresented: $showAchievement) {
+                //                ITTPAchievementsView(viewModel: achievementVM)
+            }
+            .fullScreenCover(isPresented: $showShop) {
+                //                ITTPSettingsView(settingsVM: settingsVM)
+            }
+            .fullScreenCover(isPresented: $showDailyTask) {
+                
+            }
+            .fullScreenCover(isPresented: $showDailyReward) {
+                
+            }
     }
 }
+
 
 #Preview {
     MEGMenuView()
--- Mercur Gaming/Mercur Gaming/MEGViews/MEGMenu/MEGSplashScreen.swift
+++ Mercur Gaming/Mercur Gaming/MEGViews/MEGMenu/MEGSplashScreen.swift
@@ -2,16 +2,61 @@
 //  MEGSplashScreen.swift
 //  Mercur Gaming
 //
-//  Created by Dias Atudinov on 27.06.2025.
 //
 
 import SwiftUI
 
 struct MEGSplashScreen: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.appBgMEG)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                Spacer()
+                
+                Image(.loadingLogoMEG)
+                    .resizable()
+                    .scaledToFit()
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+                Spacer()
+                
+            }
+            
+            
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
+
 
 #Preview {
     MEGSplashScreen()
