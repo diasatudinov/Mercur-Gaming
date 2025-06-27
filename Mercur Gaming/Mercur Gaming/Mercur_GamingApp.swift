d1cb899818283328a18767d43c9c854836cd6a48
Dias <dias.atudinov@gmail.com>
2025-06-27 16:15:57 +0500

    app setup

---
+++ Mercur Gaming/Mercur Gaming/Mercur_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mercur_GamingApp.swift
+//  Mercur Gaming
+//
+//  Created by Dias Atudinov on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Mercur_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
