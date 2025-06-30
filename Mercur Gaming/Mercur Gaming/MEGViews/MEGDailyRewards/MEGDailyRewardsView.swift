efc8b6371408ee38c4aebde7eca137ea030ecd53
Dias <dias.atudinov@gmail.com>
2025-06-30 20:59:42 +0500

    add daily rewards view

--- Mercur Gaming/Mercur Gaming/MEGViews/MEGDailyRewards/MEGDailyRewardsView.swift
+++ Mercur Gaming/Mercur Gaming/MEGViews/MEGDailyRewards/MEGDailyRewardsView.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct MEGDailyRewardsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var viewModel: MEGDailyRewardsViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {
+                    HStack(alignment: .top) {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        } label: {
+                            Image(.backIconMEG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 100:50)
+                        }
+                        Spacer()
+                       
+                        CoinBgPS()
+                        
+                       
+                        
+                    }.padding([.horizontal, .top])
+                }
+                
+                Image(.dailyRewardsTextMEG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 40:20)
+                    .padding(.top, 60)
+                
+                let columns = Array(repeating: GridItem(.flexible(), spacing: 0), count: 3)
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(viewModel.days, id: \.self) { day in
+                        if viewModel.currentDay.image != day.image {
+                            Image("\(day.image)BoxMEG")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 40:76)
+                        }
+                    }
+                }.padding(.top, 36)
+                
+                VStack {
+                    Text("day \(viewModel.currentDay.id)")
+                        .font(.system(size: 25, weight: .bold))
+                        .foregroundStyle(.yellow)
+                        .textCase(.uppercase)
+                    
+                    Image(.tenCoinsMEG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 150:100)
+                }.padding(.top, 36)
+                
+                Button {
+                    viewModel.takeBtnPress()
+                    
+                } label: {
+                    Image(.takeBtnMEG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ITTPDeviceInfo.shared.deviceType == .pad ? 130:70)
+                }
+                Spacer()
+            }
+        }.background(
+            ZStack {
+                Image(.appBgMEG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
     }
 }
 
 #Preview {
-    MEGDailyRewardsView()
+    MEGDailyRewardsView(viewModel: MEGDailyRewardsViewModel())
 }
--- Mercur Gaming/Mercur Gaming/MEGViews/MEGDailyRewards/MEGDailyRewardsViewModel.swift
+++ Mercur Gaming/Mercur Gaming/MEGViews/MEGDailyRewards/MEGDailyRewardsViewModel.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+class MEGDailyRewardsViewModel: ObservableObject {
+    
+    @Published var days: [DayMEG] = [
+        DayMEG(id: 1, image: "day1"),
+        DayMEG(id: 2, image: "day2"),
+        DayMEG(id: 3, image: "day3"),
+        DayMEG(id: 4, image: "day4"),
+        DayMEG(id: 5, image: "day5"),
+        DayMEG(id: 6, image: "day6"),
+        DayMEG(id: 7, image: "day7"),
+    ]
+    
+    @Published var currentDay: DayMEG = DayMEG(id: 1, image: "day1")
+    {
+        didSet {
+            saveAchievementsItem()
+        }
+    }
+    
+    init() {
+        loadAchievementsItem()
+        
+    }
+    
+    private let userDefaultsCurrentDayKey = "dailyRewards"
+
+    
+    func saveAchievementsItem() {
+        if let encodedData = try? JSONEncoder().encode(currentDay) {
+            UserDefaults.standard.set(encodedData, forKey: userDefaultsCurrentDayKey)
+        }
+        
+    }
+    
+    func loadAchievementsItem() {
+        if let savedData = UserDefaults.standard.data(forKey: userDefaultsCurrentDayKey),
+           let loadedItem = try? JSONDecoder().decode(DayMEG.self, from: savedData) {
+            currentDay = loadedItem
+        } else {
+            print("No saved data found")
+        }
+    }
+    
+    func takeBtnPress() {
+        UserPS.shared.updateUserMoney(for: 10)
+        if let index = days.firstIndex(where: { $0.image == currentDay.image }) {
+            switch index {
+            case 0:
+                currentDay = days[1]
+            case 1:
+                currentDay = days[2]
+            case 2:
+                currentDay = days[3]
+            case 3:
+                currentDay = days[4]
+            case 4:
+                currentDay = days[5]
+            case 5:
+                currentDay = days[6]
+            case 6:
+                currentDay = days[0]
+            default:
+                print("error")
+            }
+        }
+    }
+}
+
+struct DayMEG: Codable, Hashable {
+    var id: Int
+    var image: String
+}
