4fc85f1fb06ff921daa158b15ebc80311ce0dad0
Dias <dias.atudinov@gmail.com>
2025-07-01 18:18:25 +0500

    new files

---
+++ Mercur Gaming/Mercur Gaming/MEGViews/MEGGame/MEGLevelChoseView.swift
@@ -0,0 +1,18 @@
+//
+//  MEGLevelChoseView.swift
+//  Mercur Gaming
+//
+//  Created by Dias Atudinov on 01.07.2025.
+//
+
+import SwiftUI
+
+struct MEGLevelChoseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MEGLevelChoseView()
+}
